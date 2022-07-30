04af472912afe2c489933610c0dacf43a6ff52c7
James MacIvor <james@macivortech.com>
2022-07-30 16:25:24 -0400

    401 api call

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,16 @@
 import MenuItem from "./menuItem";
 
+function fetchWizzFizz() {
+  fetch('https://wizard-potion-app.azurewebsites.net/api/wizards/james@fakeemail.com~WizFizzz', { method: 'GET', redirect: 'follow' })
+  .then((response) => {
+    console.log('do what boss?')
+    if (response.status === 401) {
+      console.log('yeah boss')
+    }
+  })
+  .then((data) => console.log(data));
+}
+
 function Nav(props) {
   const menuItems = props.menuItems.map(mi => <MenuItem link={mi.link} name={mi.name}/>)
   return (
@@ -7,7 +18,7 @@ function Nav(props) {
       <ul>
         {menuItems}
         <li>
-          <a href="https://wizard-potion-app.azurewebsites.net/api/wizards/james@fakeemail.com~WizFizzz">Get WizzFizz</a>
+          <button onClick={fetchWizzFizz}>Get WizzFizz</button>
          </li>
       </ul>
     </nav>
